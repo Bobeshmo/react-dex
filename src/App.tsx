f513b66acb2cdec8b5585cf56d987b808c704b9d
Чайковский Кристиан Андреевич <ka.chaikovskiy@prisbank.com>
2022-04-13 12:54:35 +0300

    Modify routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import {SignIn} from "./pages/signIn/signIn";
+import {SignUp} from "./pages/signUp/signUp";
 import {PrivateRoute} from "./PrivateRoute";
+import {Teams} from "./pages/teams/teams";
+import {Players} from "./pages/players/players";
 import {AxiosInterceptorsSetup} from "./axios/interceptor";
-import {privateRoutes, publicRoutes} from "./router";
+import {NoMatch} from "./pages/noMatch/noMatch";
 import styles from './App.module.sass';
 
 function AxiosInterceptorNavigate() {
@@ -18,25 +22,12 @@ function App() {
             <Routes>
                 <Route element={<PrivateRoute/>}>
                     <Route path="/" element={<Navigate to="/teams"/>}/>
-                    {
-                        privateRoutes.map((route, index) => {
-                            return <Route
-                                key={route.path}
-                                path={route.path}
-                                element={<route.element/>}
-                            />
-                        })
-                    }
+                    <Route path="/teams" element={<Teams/>}/>
+                    <Route path="/players" element={<Players/>}/>
                 </Route>
-                {
-                    publicRoutes.map((route) => {
-                        return <Route
-                            key={route.path}
-                            path={route.path}
-                            element={<route.element/>}
-                        />
-                    })
-                }
+                <Route path="/login" element={<SignIn/>}/>
+                <Route path="/register" element={<SignUp/>}/>
+                <Route path="*" element={<NoMatch/>}/>
             </Routes>
         </div>
     );
